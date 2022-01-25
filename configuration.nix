2f937e9125f0abc1baa56c421a5d76549e4d8157
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2022-01-25 18:30:01 +0000

    Added cifs-tools
    Enables mounting of SMB shares

--- configuration.nix
+++ configuration.nix
@@ -88,6 +88,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #   wget
   #   firefox
+    cifs-utils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are

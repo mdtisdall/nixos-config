aaffd4d7d028df5bed9a5a7e598d25c5b0018a3f
Dylan Tisdall <mtisdall@pennmedicine.upenn.edu>
2022-04-05 13:24:20 +0000

    Added scripts for fixing undetected resolutions.

---
+++ add_resolutions.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+modeline=$(cvt -v 2560 1440 | grep Modeline | sed -e "s/Modeline //")
+echo $modeline
+xrandr --newmode ${modeline}
+modename=$(echo $modeline | awk '{ print $1; }')
+xrandr --addmode Virtual-1 $modename
+
+modeline=$(cvt -v 3024 1964 | grep Modeline | sed -e "s/Modeline //")
+echo $modeline
+xrandr --newmode ${modeline}
+modename=$(echo $modeline | awk '{ print $1; }')
+xrandr --addmode Virtual-1 $modename
